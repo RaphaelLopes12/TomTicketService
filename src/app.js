235329aee951e0e8cfab1e0e7079a217980c19df
RaphaelLopes12 <raphaellopes228@gmail.com>
2024-12-19 17:54:32 -0300

    Ajuste url to cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:4200',
+    origin: 'https://raphaelferreiralopes.com.br',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
